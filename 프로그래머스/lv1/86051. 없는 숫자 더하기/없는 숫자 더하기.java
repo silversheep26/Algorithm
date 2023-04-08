e8966a5231ed9ac655f4ced12f08e3ae7472d948
silversheep26 <122955367+silversheep26@users.noreply.github.com>
2023-04-08 17:41:20 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.06 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,13 +1,34 @@
+import java.util.ArrayList;
+
+
 class Solution {
     public int solution(int[] numbers) {
         int answer = 0;
-        int count = 0;
-        // 인덱스 값과 0~9까지 같은 숫자 찾기
-        for (int i = 0; i < numbers.length; i++) {
-            count += numbers[i];
-        }
-        answer = 45-count;
+//         int count = 0;
+//         // 인덱스 값과 0~9까지 같은 숫자 찾기
+//         for (int i = 0; i < numbers.length; i++) {
+//             count += numbers[i];
+//         }
+//         answer = 45-count;
 
+//         return answer;
+        
+        
+
+                // int to List
+        ArrayList<Integer> intList = new ArrayList<>();
+        ArrayList<Integer> notIn = new ArrayList<>();
+        for (int num : numbers) {
+            intList.add(num);
+        }
+
+        // contains 를 이용하여 안에 그 숫자가 있는지 확인
+        for (int i = 0; i < 10; i++) {
+            if (!intList.contains(i)) {
+                notIn.add(i);
+                answer += i;
+            }
+        }
         return answer;
     }
 }
