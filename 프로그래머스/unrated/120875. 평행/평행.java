ab85c81d88fea630d556e5f448e8179a8fbfbcc1
silversheep26 <122955367+silversheep26@users.noreply.github.com>
2023-10-13 23:04:56 +0900

    [level unrated] Title: 평행, Time: 0.01 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120875. 평행/평행.java
@@ -0,0 +1,22 @@
+class Solution {
+	public int solution(int[][] dots) {
+		int answer = 0;
+
+		if (((double) (dots[0][0] - dots[1][0]) / (double) (dots[0][1] - dots[1][1]))
+			== ((double) (dots[2][0] - dots[3][0]) / (double) (dots[2][1] - dots[3][1]))) {
+			answer = 1;
+		}
+
+		if (((double) (dots[0][0] - dots[2][0]) / (double) (dots[0][1] - dots[2][1]))
+			== ((double) (dots[1][0] - dots[3][0]) / (double) (dots[1][1] - dots[3][1]))) {
+			answer = 1;
+		}
+
+		if (((double) (dots[0][0] - dots[3][0]) / (double) (dots[0][1] - dots[3][1]))
+			== ((double) (dots[1][0] - dots[2][0]) / (double) (dots[1][1] - dots[2][1]))) {
+			answer = 1;
+		}
+
+		return answer;
+	}
+}
